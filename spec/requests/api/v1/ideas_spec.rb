a229c01aa8ffc78baa3e420b89bbfc8bab168850
riizu <rgbatty@outlook.com>
2016-08-15 20:54:10 -0600

    Add POST endpoint for adding ideas

--- spec/requests/api/v1/ideas_spec.rb
+++ spec/requests/api/v1/ideas_spec.rb
@@ -18,4 +18,24 @@ describe "Ideas endpoint" do
         expect(parsed_ideas.last).to_not include("updated_at")
         expect(parsed_ideas.last["title"]).to eq ideas.first.title
     end
+
+    it "sends a new idea" do
+        title = "Test title"
+        body = "test body"
+        post "/api/v1/ideas?title=#{title}&body=#{body}"
+
+        expect(response).to be_success
+
+        parsed_idea = JSON.parse(response.body)["idea"]
+        saved_idea = Idea.first
+
+        expect(parsed_idea).to_not include("created_at")
+        expect(parsed_idea).to_not include("updated_at")
+        expect(parsed_idea["title"]).to eq title
+        expect(parsed_idea["body"]).to eq body
+
+        expect(saved_idea.title).to eq title
+        expect(saved_idea.body).to eq body
+        expect(saved_idea.quality).to eq "swill"
+    end
 end
