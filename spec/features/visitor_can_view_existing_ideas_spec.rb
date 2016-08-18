ef286af4ab1142bcc560b9e54580f2ec1070e53b
riizu <rgbatty@outlook.com>
2016-08-18 13:23:14 -0600

    Fix webkit implementation

--- spec/features/visitor_can_view_existing_ideas_spec.rb
+++ spec/features/visitor_can_view_existing_ideas_spec.rb
@@ -1,19 +1,11 @@
 require 'rails_helper'
 
 RSpec.feature describe "A visitor can view existing ideas" do
-  before(:all) do
-    Capybara.current_driver = :webkit
-  end
-
-  scenario "runs something fancy with javascript" do
-      ideas = create_list(:idea, 5)
+  scenario "runs something fancy with javascript", :js => true do
+      create_list(:idea, 5)
 
       visit '/'
 
       expect(page).to have_selector('.idea', count: 5)
-  end
-
-  after(:all) do
-    Capybara.use_default_driver
   end
 end
